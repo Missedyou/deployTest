92385bd565305ff200d849e7ff4ce9401fd3afa0
dishu <3454869692@qq.com>
2018-07-13 12:03:33 +0800

    last test

--- deploy.py
+++ deploy.py
@@ -5,13 +5,13 @@ def deploy():
     add.wait()
     print('add over')
 
-    # commit = subprocess.Popen("git commit -m 'test2'", shell=True)
-    # commit.wait()
-    # print('commit over')
+    commit = subprocess.Popen("git commit -m 'last test'", shell=True)
+    commit.wait()
+    print('commit over')
 
-    # push = subprocess.Popen("git push origin master", shell=True)
-    # push.wait()
-    # print('push over')
+    push = subprocess.Popen("git push origin master", shell=True)
+    push.wait()
+    print('push over')
 
 
 if __name__ == '__main__':
