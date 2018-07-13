e570c0b1c42f90bd5606803e4b3c8c6c82dfeb3b
dishu <3454869692@qq.com>
2018-07-13 12:01:14 +0800

    test2

--- deploy.py
+++ deploy.py
@@ -1,37 +1,18 @@
 import subprocess
-from git import Repo
-
-
-RepoPath = '/Users/missyou/deployTest'
-
 
 def deploy():
-    # 执行hugo打包命令
-    # pack = subprocess.Popen("hugo --theme=prefer --baseUrl='https://missedyou.github.io/'", shell=True)
-    # pack.wait()
+    add = subprocess.Popen("git add .", shell=True)
+    add.wait()
+    print('add over')
 
-    # 进入打包后的文件夹
-    shell_list = ['cd %s'%RepoPath, 'git add .', "git commit -m 'deploy script test'", 'git push']
-    a = subprocess.Popen(shell_list, shell=True)
+    # commit = subprocess.Popen("git commit -m 'test2'", shell=True)
+    # commit.wait()
+    # print('commit over')
 
-# 暂时无法使用,, 有空研究下
-def push_to_remote():
-    repo = Repo(RepoPath)
-
-    # 切换分支
-    repo.heads.master.checkout()
-
-    # 暂存区文件处理
-    index = repo.index
-    index.add(['.'])
-    index.commit('deploy script test')
-
-    # 推送到远程
-    remote = repo.remote()
-    remote.pull()
-    remote.push()
+    # push = subprocess.Popen("git push origin master", shell=True)
+    # push.wait()
+    # print('push over')
 
 
 if __name__ == '__main__':
     deploy()
-    # push_to_remote()
